2b12939f824b7a6205c36229f68b2fb2f0fa6178
Albert Liu <albertymliu@gmail.com>
2018-10-03 17:19:58 -0400

    added pages

---
+++ src/components/pages/homepage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className = "container-fluid">
+
+		<h1>
+			Home Page Content
+		</h1>
+		<p className="App-intro">
+			Hello! This is currently not being worked on, but eventually there will be content here.
+		</p>
+
+	  </div>
+    );
+  }
+}
+export default Homepage;
---
+++ src/components/pages/page3.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Page3 extends Component {
+  render() {
+    return (
+      <div className = "container-fluid">
+
+		<h1>
+			Page3
+		</h1>
+		<p className="App-intro">
+			Hello! This is currently not being worked on, but eventually there will be content here.
+		</p>
+
+	  </div>
+    );
+  }
+}
+export default Page3;
