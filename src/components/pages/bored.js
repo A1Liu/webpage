d6f6896a2b52f61a5487dacfb975d6a39694e6e1
Albert Liu <albertymliu@gmail.com>
2018-10-04 05:05:38 -0400

    added content to bored page

--- src/components/pages/bored.js
+++ src/components/pages/bored.js
@@ -5,13 +5,36 @@ class Bored extends Component {
     return (
       <div className = "container-fluid">
 
-		<h1>
+		<h2>
 			Things to Do While Bored
-		</h1>
-		<p className="App-intro">
-			Hello! This is currently not being worked on, but eventually there will be content here.
-		</p>
+		</h2>
 
+		<ul className = "bored-list">
+			<li>
+				<a href="https://cultofthepartyparrot.com/">Cult of the Party Parrot</a>
+			</li>
+			<li>
+				<a href="http://hackertyper.com/">HackerTyper</a>
+			</li>
+			<li>
+				<a href="https://xkcd.com/">xkcd</a>
+			</li>
+			<li>
+				<a href="https://thewikigame.com/">The Wiki Game</a>
+			</li>
+			<li>
+				<a href="https://play.typeracer.com/">Type Racer</a>
+			</li>
+			<li>
+				<a href="http://www.trex-game.skipser.com/">T-Rex Game</a>
+			</li>
+			<li>
+				<a href="http://www.sanger.dk/">Pug Licking Screen</a>
+			</li>
+			<li>
+				<a href="http://drawminos.com/">Drawminos</a>
+			</li>
+		</ul>
 	  </div>
     );
   }
