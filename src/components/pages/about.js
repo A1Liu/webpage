35dcd64e9dd752b1e0fc77b66ba78b6f40227ffc
Albert Liu <albertymliu@gmail.com>
2018-10-04 20:00:06 -0400

    added more about information

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,19 +1,65 @@
 import React from 'react';
 
+const info = ["I'm Albert!",
+			"I'm a Freshman at NYU majoring in Math and Computer Science.",
+			"I'm from Pittsburgh, Pennsylvania.",
+			"I went to Fox Chapel Area High School.",
+			(<span>My github is&nbsp;<a href = "https://github.com/A1Liu">https://github.com/A1Liu</a></span>),
+			];
+
+class AboutInfo extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<p className = "about-info">
+				{info[this.props.elem]}
+			</p>
+		);
+	}
+}
+
 class About extends React.Component {
-  render() {
-    return (
-      <div className = "container-fluid">
 
-		<h1>
-			About Me
-		</h1>
-		<p className="App-intro">
-			I'm Albert!
-		</p>
+	constructor(props) {
+		super(props);
+		this.state = {counter: 0};
+		this.incr_count = this.incr_count.bind(this);
+	}
 
-	  </div>
-    );
-  }
+
+
+	incr_count() {
+		var i = Math.floor(Math.random() * info.length);
+		this.setState((state) =>({ counter: ((i == state.counter) ? (i+1) % info.length: i) }));
+	}
+
+	render() {
+		return (
+			<div className = "container-fluid">
+
+				<h1>
+					About Me
+				</h1>
+
+				< AboutInfo elem={this.state.counter}/>
+
+				<p>
+					Check out my
+					<button className="btnText">
+						<a onClick={this.incr_count}>
+							&nbsp;About Me&nbsp;
+						</a>
+					</button>
+
+					page for more info!
+				</p>
+
+			</div>
+		);
+	}
 }
 export default About;
