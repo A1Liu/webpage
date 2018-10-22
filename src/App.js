c830dce62f46588147d5697f79e21fbd8cfc85ab
Albert Liu <albertymliu@gmail.com>
2018-10-22 16:26:41 -0400

    idk man

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import {
 } from 'react-router-dom';
 
 //CSS Style sheets
-import './Assets/css/default.min.css';
-import './Assets/css/bored.min.css';
-import './Assets/css/about.min.css';
+import './assets/css/default.min.css';
+import './assets/css/bored.min.css';
+import './assets/css/about.min.css';
 
 // Header and footer
 import Header from './components/headerComponent/header';
@@ -25,17 +25,19 @@ class App extends React.Component {
   render() {
     return (
 		<Router>
-		<div className="App">
+		<div className="app">
 
 			<Header />
 
-			<div className = "container-fluid">
-				<Route exact path = '/' component={Homepage} />
-				<Route exact path = '/about' component={About} />
-				<Route exact path = '/projects' component={Projects} />
-				<Route exact path = '/bored' component={Bored} />
-				<Route exact path = '/resources' component={Resources} />
-			</div>
+				<article className = "main">
+					<Route exact path = '/' component={Homepage} />
+					<Route exact path = '/about' component={About} />
+					<Route exact path = '/projects' component={Projects} />
+					<Route exact path = '/bored' component={Bored} />
+					<Route exact path = '/resources' component={Resources} />
+				</article>
+				<aside className = "aside aside-1"></aside>
+				<aside className = "aside aside-2"></aside>
 
 			<Footer />
 
@@ -44,5 +46,15 @@ class App extends React.Component {
     );
   }
 }
+
+/* <div class="wrapper">
+  <header class="header">Header</header>
+  <article class="main">
+    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+  </article>
+  <aside class="aside aside-1">Aside 1</aside>
+  <aside class="aside aside-2">Aside 2</aside>
+  <footer class="footer">Footer</footer>
+</div> */
 
 export default App;
